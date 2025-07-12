0a461c5e8028d3784b5e078dbe4eee476763d702
sdandge <saitej_dandge@intuit.com>
2025-07-12 18:03:49 +0530

    Linked blocking queue

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    // For most Kotlin projects (JVM, Native)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
     implementation("mysql:mysql-connector-java:8.0.33")
     testImplementation(kotlin("test"))
 }
